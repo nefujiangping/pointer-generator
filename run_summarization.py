8943d56232f4f92111f6560cf0a9ff868d3eb6f8
nefujiangping <825617010@qq.com>
2018-04-16 20:20:53 +0800

    add log to file.

--- run_summarization.py
+++ run_summarization.py
@@ -29,6 +29,8 @@ from decode import BeamSearchDecoder
 import util
 from tensorflow.python import debug as tf_debug
 
+import logging
+
 FLAGS = tf.app.flags.FLAGS
 
 # Where to find data
@@ -48,9 +50,9 @@ tf.app.flags.DEFINE_integer('hidden_dim', 256, 'dimension of RNN hidden states')
 tf.app.flags.DEFINE_integer('emb_dim', 128, 'dimension of word embeddings')
 tf.app.flags.DEFINE_integer('batch_size', 16, 'minibatch size')
 tf.app.flags.DEFINE_integer('max_enc_steps', 400, 'max timesteps of encoder (max source text tokens)')
-tf.app.flags.DEFINE_integer('max_dec_steps', 100, 'max timesteps of decoder (max summary tokens)')
+tf.app.flags.DEFINE_integer('max_dec_steps', 20, 'max timesteps of decoder (max summary tokens)')
 tf.app.flags.DEFINE_integer('beam_size', 4, 'beam size for beam search decoding.')
-tf.app.flags.DEFINE_integer('min_dec_steps', 35, 'Minimum sequence length of generated summary. Applies only for beam search decoding mode')
+tf.app.flags.DEFINE_integer('min_dec_steps', 3, 'Minimum sequence length of generated summary. Applies only for beam search decoding mode')
 tf.app.flags.DEFINE_integer('vocab_size', 50000, 'Size of vocabulary. These will be read from the vocabulary file in order. If the vocabulary file contains fewer words than this number, or if this number is set to 0, will take all words in the vocabulary file.')
 tf.app.flags.DEFINE_float('lr', 0.15, 'learning rate')
 tf.app.flags.DEFINE_float('adagrad_init_acc', 0.1, 'initial accumulator value for Adagrad')
@@ -265,10 +267,26 @@ def run_eval(model, batcher, vocab):
     if train_step % 100 == 0:
       summary_writer.flush()
 
+def init_logging(log_file):
+  """Init for logging
+  """
+  logging.basicConfig(
+                    level    = logging.DEBUG,
+                    format='%(asctime)s-%(levelname)s-%(message)s',
+                    datefmt  = '%y-%m-%d %H:%M',
+                    filename = log_file,
+                    filemode = 'w');
+  console = logging.StreamHandler()
+  console.setLevel(logging.INFO)
+  formatter = logging.Formatter('%(asctime)s-%(levelname)s-%(message)s')
+  console.setFormatter(formatter)
+  logging.getLogger('').addHandler(console)
 
 def main(unused_argv):
   assert FLAGS.gpu_no != '' , 'gpu_no must be assigned!'
   os.environ["CUDA_VISIBLE_DEVICES"] = FLAGS.gpu_no
+
+  init_logging(FLAGS.mode +'.log-' + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
   if len(unused_argv) != 1: # prints a message if you've entered flags incorrectly
     raise Exception("Problem with flags: %s" % unused_argv)
